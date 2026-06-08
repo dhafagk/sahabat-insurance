509975b77c527fc17d9e17473b4f447f07ff2923
dhafagk <dhafaggtq@gmail.com>
2026-06-09 00:50:28 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,6 @@
 import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "standalone",
-  webpack: (config, { dev }) => {
-    if (dev) {
-      config.watchOptions = { poll: 1000, aggregateTimeout: 300 };
-    }
-    config.output.filename = config.output.filename.replace(
-      "[chunkhash]",
-      "[contenthash]",
-    );
-    config.output.chunkFilename = config.output.chunkFilename.replace(
-      "[chunkhash]",
-      "[contenthash]",
-    );
-    return config;
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default withPayload(nextConfig);
