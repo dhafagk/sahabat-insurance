e779adab63d8b6d8878a87bf3aba37cba1a37506
dhafagk <dhafaggtq@gmail.com>
2026-06-09 13:45:47 +0700

    update

--- migrations/index.ts
+++ migrations/index.ts
@@ -1,6 +1,12 @@
+import * as migration_20260608_174533 from './20260608_174533';
 import * as migration_20260609_061054 from './20260609_061054';
 
 export const migrations = [
+  {
+    up: migration_20260608_174533.up,
+    down: migration_20260608_174533.down,
+    name: '20260608_174533'
+  },
   {
     up: migration_20260609_061054.up,
     down: migration_20260609_061054.down,
